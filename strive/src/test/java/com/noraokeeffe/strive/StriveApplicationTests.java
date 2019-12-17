d9a3d27fe2d30a95ad3fbc53cfd21d7ee60f3e8e
NoraOK <n.okeeffe35@gmail.com>
2019-12-17 11:53:32 -0800

    first commit

---
+++ strive/src/test/java/com/noraokeeffe/strive/StriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noraokeeffe.strive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
